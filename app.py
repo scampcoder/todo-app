0304dabe6ed397560f47c08139f90cfd698f7d35
Victoria Kee <victoriakee95@gmail.com>
2021-02-17 11:18:55 -0600

    Redirect home to list one for now

--- app.py
+++ app.py
@@ -74,7 +74,11 @@ def delete_todo(todo_id):
 
 
 @app.route('/lists/<list_id>')
-def index():
+def get_list_todos(list_id):
     return render_template('index.html', 
     data=Todo.query.filter_by(list_id=list_id).order_by('id').all()
     )
+
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id=1))
