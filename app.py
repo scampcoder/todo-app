33d2ca9ec7c694ca6c2df9c76bbeb7971e1e6f2e
Victoria Kee <victoriakee95@gmail.com>
2021-02-17 11:32:49 -0600

    Add 'lists' to homepg

--- app.py
+++ app.py
@@ -76,7 +76,8 @@ def delete_todo(todo_id):
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template('index.html', 
-    data=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    lists=TodoList.query.all(),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
     )
 
 @app.route('/')
