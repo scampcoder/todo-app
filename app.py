f1fc1cae2ca2f75f70003f2028c6cc9f3492feb9
Victoria Kee <victoriakee95@gmail.com>
2021-02-17 11:49:01 -0600

    Give lists Headers with Active List name

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def delete_todo(todo_id):
 def get_list_todos(list_id):
     return render_template('index.html', 
     lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id),
     todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
     )
 
